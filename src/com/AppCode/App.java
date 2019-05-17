ee6e62b23e20b384ad4fefcfaff651078190f11c
Mario <codingphs@gmail.com>
2019-05-17 10:42:47 -0500

    Moved enroll to enrollStudent method

--- src/com/AppCode/App.java
+++ src/com/AppCode/App.java
@@ -1,10 +1,11 @@
 package com.AppCode;
 
-
 import java.sql.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+
 
 public class App {
     //panel
@@ -24,25 +25,22 @@ public class App {
     private JRadioButton juniorRadioButton;
     private JRadioButton seniorRadioButton;
 
+
     public App()
     {
-
-        buttonMsg.addActionListener(new ActionListener() {
+        buttonMsg.addActionListener(new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent actionEvent)
             {
 
+                //TODO: clean GUI
 
                 //mySQL connection info
                 String host = "jdbc:mysql://localhost:3306/students";
                 String user = "root";
                 String pass = "torres";
 
-
-                //TODO: clean GUI
-                //TODO: add courses functionality
-
-                //grab ID
 
                 //grab text from inputs
                 String first = firstNameTextField.getText();
@@ -51,15 +49,12 @@ public class App {
 
 
                 //Radio button group selection
-                if (freshmanRadioButton.isSelected()) {
-                    classification = "Freshman";
-                } else if (sophomoreRadioButton.isSelected()) {
-                    classification = "Sophomore";
-                } else if (juniorRadioButton.isSelected()) {
-                    classification = "Junior";
-                } else if (seniorRadioButton.isSelected()) {
-                    classification = "Senior";
+                if (freshmanRadioButton.isSelected()) { classification = "Freshman";
+                } else if (sophomoreRadioButton.isSelected()) { classification = "Sophomore";
+                } else if (juniorRadioButton.isSelected()) { classification = "Junior";
+                } else if (seniorRadioButton.isSelected()) { classification = "Senior";
                 }
+
 
                 //get student ID
                 studentID = generateID(host, user, pass);
@@ -68,60 +63,63 @@ public class App {
                 //if first/last name are empty
                 //if classification is unselected
                 if (firstNameTextField.getText().trim().length() == 0 || lastNameTextField.getText().trim().length() == 0 || classification == "empty") {
-                    //then display error
-                    displayError();
+                    displayError(); //then display error
                 } else {
-                    //display output
-                    displayOutput(first, last);
-                    //getSQLStatement(firstNameTextField.getText(), lastNameTextField.getText(), classification, major);
+                    displayOutput(first, last); //display output
                 }
 
+                //Enroll user
+                enrollUser(host, user, pass, first, last, major, classification);
 
-                //SQL
-                try {
-                    Connection connection = DriverManager.getConnection(host, user, pass);
-                    System.out.println("Connected!");
-                    Statement statement = connection.createStatement();
-
-                    String query = " insert into users (studentID, firstName, lastName, Major, Classification)"
-                            + " values (?, ?, ?, ?, ?)";
-
-                    String rowCountQuery = "SELECT COUNT(*) FROM users";
-
-                    PreparedStatement preparedStatement = connection.prepareStatement(query);
-                    preparedStatement.setInt(1, studentID);
-                    preparedStatement.setString(2, first);
-                    preparedStatement.setString(3, last);
-                    preparedStatement.setString(4, major);
-                    preparedStatement.setString(5, classification);
-
-
-                    try {
-                        preparedStatement.execute();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        connection.close();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-
-                } catch (SQLException error) {
-                    error.printStackTrace();
-                    System.out.println(error.getMessage());
-                }
-            }//action performed ^^
+            }
 
         });//action listener
 
     }//app
 
 
+
+    // Enrolls student into DB //
+    private void enrollUser(String host, String user, String pass, String first, String last, String major, String classification)
+    {
+        try
+        {
+            Connection connection = DriverManager.getConnection(host, user, pass);
+            System.out.println("Connected!");
+            Statement statement = connection.createStatement();
+
+            String query = " insert into users (studentID, firstName, lastName, Major, Classification)"
+                    + " values (?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, studentID);
+            preparedStatement.setString(2, first);
+            preparedStatement.setString(3, last);
+            preparedStatement.setString(4, major);
+            preparedStatement.setString(5, classification);
+
+
+            try {
+                preparedStatement.execute(); //execute
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close(); //close connection
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException error)
+        {
+            error.printStackTrace();
+            System.out.println(error.getMessage()); //error message
+        }
+    }//action performed ^^
+
+
     public static void main(String[] args)
     {
-
-
         //JFrame stuff
         JFrame frame = new JFrame("Student Registration");
         frame.setContentPane(new App().panelMain);
@@ -130,9 +128,10 @@ public class App {
         frame.pack();
         frame.setVisible(true);
 
-
+        //Create different JFrame windows for different user functions.
     }
 
+    // Outputs generic error message //
     public void displayError()
     {
         //request user to select a classification.
@@ -142,17 +141,19 @@ public class App {
         dialog.setVisible(true);
     }
 
+    //Output student information - maybe not necessary //
+    //TODO: revise
     public void displayOutput(String first, String last)
     {
         JOptionPane.showMessageDialog(null, "Name: " + first + " " + last +
                 "\nClassification: " + classification);
     }
 
-    //not how actual ID's could be generated. since everyone starts off as a freshman
-    //TODO: redo logic
-    public int generateID(String host, String user, String pass)
+    // Generate studentID - gets row number from table and that = studentID //
+    private int generateID(String host, String user, String pass)
     {
 
+        //A lot of try/catch but it didn't work without it so ¯\_(ツ)_/¯
         int rowNumber = 0;
 
         Connection connection = null;
@@ -199,6 +200,4 @@ public class App {
 
     }
 
-
-//
 }
